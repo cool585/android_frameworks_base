PixysOS/frameworks_base

PixelPropUtils: Un-spoof GMS while adding google account
GMS uses its DroidGuard service (the same that's used for safetynet)
to get the device name from the build fingerprint, which from our
current certified props, is Pixel XL. The device name after Google
login shows up in the login alert accordingly as Pixel XL, which is
inconvenient especially when you have multiple devices on the same ROM.

By restarting the DroidGuard service process (gms.unstable) as soon as
the user starts adding Google account (i.e when the GMS add account
activity is started and moved to the top of the task stack), we can
skip spoofing to certified props in this instance. When the user
stops/finishes adding account (GMS add account activity is no longer
at the top of the task stack), restart the process again, this time
spoofing to certified props in order to be ready for safetynet.

This ensures while adding a Google account, GMS reads the original
properties of the device and labels the device with its original name,
while in any other situation GMS reads certified properties required
to pass SafetyNet/Play Integrity.

Test:
- Add google account from setup wizard, settings, any other app
- Observe correct device name in gmail login alert
- SafetyNet (basic, CTS), Play Integrity (basic, device) passes.

[aswin7469]: Adapted for PixelPropUtils

Change-Id: Ia5463f220a8262f7963fc5b2640a7a5311a73210
Signed-off-by: aswin7469 <aswinas@pixysos.com>

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -272,6 +272,7 @@ import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.usage.AppStandbyInternal;
 
 import com.android.internal.util.pixys.cutout.CutoutFullscreenController;
+import com.android.internal.util.pixys.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1975,7 +1976,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -2996,7 +2999,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 

cool585/android_frameworks_base

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -271,6 +271,7 @@ import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.usage.AppStandbyInternal;
 
 import com.android.internal.util.custom.cutout.CutoutFullscreenController;
+import com.android.internal.util.superior.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1971,7 +1972,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3001,7 +3004,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 